eca30a0b4370270f89d6ff6d209ed73bd62afc2c
Dominik Stumpf <s.dominik.work@gmail.com>
2023-12-02 15:22:54 +0100

    feat: solve day-02/problem-a

---
+++ typescript/year-2023/day-02/solution/problem-b.ts
@@ -0,0 +1,12 @@
+const _exampleFile = Bun.file('input/example-a');
+const inputFile = Bun.file('input/input-a');
+
+async function main() {
+  const input = await inputFile.text();
+  const inputLines = input.split('\n');
+
+  inputLines.pop(); // omit last trailing empty line
+
+}
+
+main();
