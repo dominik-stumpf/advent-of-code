c884dabe61a77c4598a058eb516298014290ee0c
Dominik Stumpf <s.dominik.work@gmail.com>
2023-12-04 16:09:36 +0100

    feat: solve day-04/problem-a

--- typescript/year-2023/day-04/solution/problem-a.ts
+++ typescript/year-2023/day-04/solution/problem-a.ts
@@ -1,15 +1,56 @@
 const _exampleFile = Bun.file('input/example-a');
-const _inputFile = Bun.file('input/input-a');
+const inputFile = Bun.file('input/input-a');
+
+interface ScratchCard {
+  winningNumbers: number[];
+  receivedNumbers: number[];
+}
+
+function parseCard(cardLine: string): ScratchCard {
+  const [winningNumbers, receivedNumbers] = cardLine
+    .slice(cardLine.indexOf(': ') + 1)
+    .split('|')
+    .map((numbers) => {
+      return numbers
+        .trim()
+        .split(/ +/)
+        .map((number) => Number(number));
+    });
+
+  return { winningNumbers, receivedNumbers };
+}
+
+function evaluateScratchCardPoint({
+  winningNumbers,
+  receivedNumbers,
+}: ScratchCard): number {
+  let matchCount = 0;
+
+  for (const winningNumber of winningNumbers) {
+    if (receivedNumbers.includes(winningNumber)) {
+      matchCount += 1;
+    }
+  }
+
+  return matchCount && 2 ** (matchCount - 1);
+}
 
 async function main() {
-  const input = await _exampleFile.text();
+  const input = await inputFile.text();
   const inputLines = input.split('\n');
 
   inputLines.pop(); // omit last trailing empty line
 
+  let sum = 0;
+
   for (const line of inputLines) {
-    console.log(line);
+    const parsedCard = parseCard(line);
+    const cardPoint = evaluateScratchCardPoint(parsedCard);
+
+    sum += cardPoint;
   }
+
+  console.log('sum:', sum);
 }
 
 main();
