9ca565d5f7362bce0a9f49f0c48d3947733087d2
Dominik Stumpf <s.dominik.work@gmail.com>
2023-12-06 18:34:02 +0100

    chore: add migation helper

---
+++ typescript/year-2023/migrator.ts
@@ -0,0 +1,31 @@
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from 'node:fs';
+
+function main() {
+  for (let dayIndex = 1; dayIndex <= 25; dayIndex += 1) {
+    const day = dayIndex.toString().padStart(2, '0');
+
+    for (let part = 1; part <= 2; part += 1) {
+      const partLetter = 'ab'[part - 1];
+      const oldInput = `day-${day}/input/input-${partLetter}`;
+      const oldExample = `day-${day}/input/example-${partLetter}`;
+
+      const input = `day-${day}/input/input-part-${part}`;
+      const example = `day-${day}/input/example-part-${part}`;
+
+      if (existsSync(oldInput)) {
+        const oldFile = readFileSync(oldInput, 'utf-8')
+        writeFileSync(input, oldFile, {});
+        unlinkSync(oldInput);
+      }
+      if (existsSync(oldExample)) {
+        const oldFile = readFileSync(oldExample, 'utf-8')
+        writeFileSync(example, oldFile, {});
+        unlinkSync(oldExample);
+      }
+    }
+  }
+
+  console.log('migration done');
+}
+
+main();
