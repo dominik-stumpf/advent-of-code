ab939bacb52f312810227de713d76b4e62861ee5
Dominik Stumpf <s.dominik.work@gmail.com>
2023-12-06 23:36:00 +0100

    feat: solve day-06/problem-part-2

--- typescript/year-2023/day-06/solution/problem-part-2.ts
+++ typescript/year-2023/day-06/solution/problem-part-2.ts
@@ -1,4 +1,34 @@
-// biome-ignore lint: solution not implmeneted
 export function solveProblem(input: string): number {
-  throw new Error('solution not implemented');
+  const race = parseRace(input);
+  let newDistanceRecordCounter = 0;
+
+  for (let i = 0; i < race.timeBudget; i += 1) {
+    const dist = calcDistanceTraveled(race.timeBudget, i);
+    if (dist > race.distanceTraveled) {
+      newDistanceRecordCounter += 1;
+    }
+  }
+
+  return newDistanceRecordCounter;
+}
+
+interface Race {
+  timeBudget: number;
+  distanceTraveled: number;
+}
+
+function parseRace(rawRace: string): Race {
+  const [timeBudget, distanceTraveled] = rawRace
+    .split('\n')
+    .slice(0, -1)
+    .map((row) => Number(row.split(':')[1].trim().split(/ +/).join('')));
+
+  return {
+    distanceTraveled,
+    timeBudget,
+  };
+}
+
+function calcDistanceTraveled(timeBudget: number, holdTime: number) {
+  return (timeBudget - holdTime) * holdTime;
 }
