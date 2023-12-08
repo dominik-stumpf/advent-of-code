71f9d60d6a322ff5b7f311af573493f5d80ef36b
Dominik Stumpf <s.dominik.work@gmail.com>
2023-12-08 12:31:05 +0100

    feat: try solving problem part 2

--- typescript/year-2023/day-07/solution/problem-part-2.ts
+++ typescript/year-2023/day-07/solution/problem-part-2.ts
@@ -1,4 +1,224 @@
-// biome-ignore lint: solution not implmeneted
 export function solveProblem(input: string): number {
-  throw new Error('solution not implemented');
+  const hands = parseHands(input);
+  const evaluatedHands = hands.map((hand) => evaluateHand(hand));
+  const rankedHands = rankHands(evaluatedHands);
+
+  Bun.write('./day-07/solution/rankedHands.json', JSON.stringify(rankedHands));
+
+  return rankedHands.reduce(
+    (accumulator, hand) => accumulator + hand.bid * hand.rank,
+    0,
+  );
+}
+const cardLabels = [
+  'A',
+  'K',
+  'Q',
+  'T',
+  '9',
+  '8',
+  '7',
+  '6',
+  '5',
+  '4',
+  '3',
+  '2',
+  'J',
+] as const;
+type CardLabel = typeof cardLabels[number];
+
+interface Hand {
+  bid: number;
+  cards: string;
+}
+
+function parseHands(rawHands: string): Hand[] {
+  const hands = rawHands
+    .split('\n')
+    .slice(0, -1)
+    .map((hand) => hand.split(' '))
+    .map(([cards, bid]) => ({ cards, bid: Number(bid) }));
+  return hands;
+}
+
+const handTypes = [
+  'fiveOfAKind',
+  'fourOfAKind',
+  'fullHouse',
+  'threeOfAKind',
+  'twoPair',
+  'onePair',
+  'highCard',
+] as const;
+
+type HandType = typeof handTypes[number];
+type LabelCount = Record<CardLabel, number>;
+type HandTypeMapCallback = (cards: string, labelCount: LabelCount) => boolean;
+
+interface HandStrength {
+  type: HandType;
+}
+
+interface EvaluatedHand extends HandStrength, Hand { }
+
+const handTypeMap: Record<HandType, HandTypeMapCallback> = {
+  fiveOfAKind: (_, labelCount) => {
+    const jokerCount = labelCount.J;
+    for (const [label, count] of Object.entries(labelCount)) {
+      if (label === 'J') {
+        continue;
+      }
+      if (count + jokerCount === 5) {
+        return true;
+      }
+    }
+
+    return false;
+  },
+  fourOfAKind: (_, labelCount) => {
+    const jokerCount = labelCount.J;
+    for (const [label, count] of Object.entries(labelCount)) {
+      if (label === 'J') {
+        continue;
+      }
+      if (jokerCount + count === 4) {
+        return true;
+      }
+    }
+
+    return false;
+  },
+  fullHouse: (_, labelCount) => {
+    // let threeLabel: undefined | CardLabel;
+    // let twoLabel: undefined | CardLabel;
+    const jokerCount = labelCount.J;
+    const labelValues = Object.entries(labelCount)
+      .filter(([key]) => key !== 'J')
+      .map(([, value]) => value);
+
+    if (
+      jokerCount === 1 &&
+      labelValues.filter((label) => label === 2).length === 2
+    ) {
+      return true;
+    }
+
+    // for (const [label, count] of Object.entries(labelCount)) {
+    //   if (label === 'J') {
+    //     continue;
+    //   }
+    //   if (cards === 'JQ24J') {
+    //     console.log(threeLabel, twoLabel, jokerCount);
+    //   }
+    //
+    //   // if (!threeLabel && count + jokerCount === 3) {
+    //   //   threeLabel = label as CardLabel;
+    //   //   jokerCount = Math.max(0, jokerCount - count);
+    //   // } else if (!twoLabel && count + jokerCount === 2) {
+    //   //   twoLabel = label as CardLabel;
+    //   //   jokerCount = Math.max(0, jokerCount - count);
+    //   // }
+    // }
+
+    // if (threeLabel && twoLabel) {
+    //   if (cards === 'JQ24J') {
+    //     console.error('incorrect fullHouse');
+    //   }
+    //   return true;
+    // }
+
+    //   if (cards === 'JQ24J') {
+    //     console.error('incorrect fullHouse');
+    //   }
+    return false;
+  },
+  threeOfAKind: (_, labelCount) => {
+    const jokerCount = labelCount.J;
+    for (const [label, count] of Object.entries(labelCount)) {
+      if (label === 'J') {
+        continue;
+      }
+      if (count + jokerCount === 3) {
+        return true;
+      }
+    }
+    return false;
+  },
+  twoPair: (_, labelCount) => {
+    const labelValues = Object.entries(labelCount)
+      .filter(([key]) => key !== 'J')
+      .map(([, value]) => value);
+
+    if (
+      labelValues.filter((label) => label === 2).length === 2
+    ) {
+      return true;
+    }
+
+    return false;
+  },
+  onePair: (_, labelCount) => {
+    const jokerCount = labelCount.J;
+    for (const [label, count] of Object.entries(labelCount)) {
+      if (label === 'J') {
+        continue;
+      }
+      if (count + jokerCount === 2) {
+        return true;
+      }
+    }
+    return false;
+  },
+  highCard: () => {
+    return true;
+  },
+};
+
+function evaluateHand(hand: Hand): EvaluatedHand {
+  const labelCount = Object.fromEntries(
+    cardLabels.map((label) => [label, 0]),
+  ) as LabelCount;
+
+  for (let i = 0; i < hand.cards.length; i += 1) {
+    labelCount[hand.cards[i] as CardLabel] += 1;
+  }
+
+  for (const handType of handTypes) {
+    const evaluatedHand = handTypeMap[handType](hand.cards, labelCount);
+    if (evaluatedHand) {
+      return { ...hand, type: handType };
+    }
+  }
+
+  throw new Error('cards could not be evaluated');
+}
+
+interface RankedHand extends EvaluatedHand {
+  rank: number;
+}
+
+function rankHands(evaluatedHands: EvaluatedHand[]): RankedHand[] {
+  const result: RankedHand[] = [];
+
+  let rankCount = evaluatedHands.length;
+
+  for (const handType of handTypes) {
+    const hands = evaluatedHands.filter((hand) => hand.type === handType);
+    hands.sort((a, b) => {
+      for (let i = 0; i < a.cards.length; i += 1) {
+        const [labelA, labelB] = [a.cards[i], b.cards[i]].map((label) =>
+          cardLabels.indexOf(label as CardLabel),
+        );
+        if (labelA === labelB) {
+          continue;
+        }
+        return labelA - labelB;
+      }
+
+      throw new Error('could not sort card');
+    });
+    result.push(...hands.map((hand) => ({ ...hand, rank: rankCount-- })));
+  }
+
+  return result;
 }
