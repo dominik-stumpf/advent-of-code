2ef0c45458bc799ae9946205bd1ae15418f49147
Dominik Stumpf <s.dominik.work@gmail.com>
2023-12-11 18:49:46 +0100

    feat: try solving problem part 2

--- typescript/year-2023/day-08/solution/problem-part-2.ts
+++ typescript/year-2023/day-08/solution/problem-part-2.ts
@@ -1,4 +1,59 @@
-// biome-ignore lint: solution not implmeneted
 export function solveProblem(input: string): number {
-  throw new Error('solution not implemented');
+  const map = parseMap(input);
+  const steps = countTraversalSteps(map);
+
+  return steps;
+}
+
+interface Map {
+  instructions: string;
+  nodes: Record<string, [string, string]>;
+}
+
+function parseMap(rawMap: string): Map {
+  const [instructions, rawNodes] = rawMap.trim().split('\n\n');
+  const nodes = Object.fromEntries(
+    rawNodes
+      .split('\n')
+      .map((node) => node.split(' = '))
+      .map(([currentNode, next]) => [
+        currentNode,
+        next.slice(1, -1).split(', '),
+      ]),
+  );
+
+  return { instructions, nodes: nodes as Map['nodes'] };
+}
+
+function countTraversalSteps({ nodes, instructions }: Map): number {
+  let stepCount = 0;
+  let instructionIndex = 0;
+  const nodeKeys = Object.keys(nodes);
+  const originNodes = nodeKeys.filter((key) => key.at(-1) === 'A');
+  const currentNodes = originNodes;
+  const instructionIndices = instructions
+    .split('')
+    .map((instruction) => (instruction === 'L' ? 0 : 1));
+  let time = performance.now();
+
+  do {
+    if (stepCount % 1e7 === 0) {
+      const newTime = performance.now();
+      console.log('step:', stepCount, 'elapsed:', newTime - time);
+      time = newTime;
+    }
+
+    const instruction = instructionIndices[instructionIndex];
+    let nodeIndex = 0;
+
+    for (const node of currentNodes) {
+      currentNodes[nodeIndex] = nodes[node][instruction];
+      nodeIndex += 1;
+    }
+
+    instructionIndex = (instructionIndex + 1) % instructionIndices.length;
+    stepCount += 1;
+  } while (!currentNodes.every((node) => node.at(-1) === 'Z'));
+
+  return stepCount;
 }
