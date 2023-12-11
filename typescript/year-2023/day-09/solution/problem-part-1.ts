4d5e33acc322c65aab4441ff86c3ae6f1d6a88ae
Dominik Stumpf <s.dominik.work@gmail.com>
2023-12-12 00:15:10 +0100

    feat: solve day-09/problem-part-1

--- typescript/year-2023/day-09/solution/problem-part-1.ts
+++ typescript/year-2023/day-09/solution/problem-part-1.ts
@@ -1,4 +1,37 @@
-// biome-ignore lint: solution not implmeneted
 export function solveProblem(input: string): number {
-  throw new Error('solution not implemented');
+  const histories = parseHistories(input);
+
+  return histories.reduce(
+    (accumulator, history) => accumulator + extrapolateHistory(history),
+    0,
+  );
+}
+
+function parseHistories(rawHistory: string): number[][] {
+  return rawHistory
+    .trim()
+    .split('\n')
+    .map((line) => line.split(' ').map(Number));
+}
+
+function extrapolateHistory(history: number[]): number {
+  let prediction = history[history.length - 1];
+  let nextRow = history;
+
+  while (!nextRow.every((value, _, array) => array[0] === value)) {
+    nextRow = createNextRow(nextRow);
+    prediction += nextRow[nextRow.length - 1];
+  }
+
+  return prediction;
+}
+
+function createNextRow(currentRow: number[]): number[] {
+  const nextRow: number[] = [];
+
+  for (let i = 0; i < currentRow.length - 1; i += 1) {
+    nextRow.push(currentRow[i + 1] - currentRow[i]);
+  }
+
+  return nextRow;
 }
