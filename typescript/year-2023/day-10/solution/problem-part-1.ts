2c7fdb05cee4bf2d00260841bad858851fc9b2dd
Dominik Stumpf <s.dominik.work@gmail.com>
2023-12-12 23:16:24 +0100

    feat: solve day-10/problem-part-1

--- typescript/year-2023/day-10/solution/problem-part-1.ts
+++ typescript/year-2023/day-10/solution/problem-part-1.ts
@@ -19,9 +19,9 @@ function parsePipeGrid(rawPipeGrid: string): PipeGrid {
     .split('\n')
     .map((row) => row.split(''));
   let startPosition: Vec2 | undefined;
-  for (let x = 0; x < grid.length; x += 1) {
-    const y = grid[x].findIndex((pipe) => pipe === 'S');
-    if (y !== -1) {
+  for (let y = 0; y < grid.length; y += 1) {
+    const x = grid[y].findIndex((pipe) => pipe === 'S');
+    if (x !== -1) {
       startPosition = { x, y };
     }
   }
@@ -33,68 +33,65 @@ function parsePipeGrid(rawPipeGrid: string): PipeGrid {
   return { grid, startPosition };
 }
 
-const allDirections = ['up', 'right', 'down', 'left'] as const;
+type Direction = 'up' | 'right' | 'down' | 'left';
 
-type Direction = (typeof allDirections)[number];
-
-const pipeMap: Record<string, [Direction, Direction]> = {
+const pipeMap: Record<string, Direction[]> = {
   '|': ['up', 'down'],
   '-': ['left', 'right'],
-  L: ['up', 'left'],
-  J: ['up', 'right'],
+  L: ['up', 'right'],
+  J: ['up', 'left'],
   '7': ['left', 'down'],
   F: ['right', 'down'],
+  S: ['right', 'down', 'up', 'left'],
 } as const;
 
-function findValidAdjacentPipes(
-  grid: PipeGrid['grid'],
-  position: Vec2,
-): Vec2[] {
-  const currentDirections =
-    pipeMap[grid[position.x][position.y]] ?? allDirections;
-
-  return [
-    { x: position.x + 1, y: position.y },
-    { x: position.x - 1, y: position.y },
-    { x: position.x, y: position.y + 1 },
-    { x: position.x, y: position.y - 1 },
-  ].filter((potentialPosition) => {
-    if (potentialPosition.x < 0 || potentialPosition.y < 0) {
-      return false;
-    }
-
-    const pipe = grid.at(potentialPosition.x)?.at(potentialPosition.y);
-    const directions = pipe ? pipeMap[pipe] : undefined;
-    if (directions === undefined) {
-      return false;
-    }
-
-    for (const direction of directions) {
-      for (const currentDirection of currentDirections) {
-        if (checkIfPathable(currentDirection, direction)) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  });
+interface AdjacentPipe extends Vec2 {
+  direction: Direction;
 }
 
-function checkIfPathable(from: Direction, to: Direction): boolean {
-  return (
-    (from === 'left' && to === 'right') ||
-    (from === 'right' && to === 'left') ||
-    (from === 'up' && to === 'down') ||
-    (from === 'down' && to === 'up')
-  );
+const oppositeDirection: Record<Direction, Direction> = {
+  left: 'right',
+  right: 'left',
+  up: 'down',
+  down: 'up',
+} as const;
+
+function findValidAdjacentPipes(grid: PipeGrid['grid'], position: Vec2) {
+  const currentPipe = grid[position.y][position.x];
+  const currentDirections = pipeMap[currentPipe];
+
+  let adjacentPipes: AdjacentPipe[] = [
+    { x: position.x + 1, y: position.y, direction: 'right' },
+    { x: position.x - 1, y: position.y, direction: 'left' },
+    { x: position.x, y: position.y + 1, direction: 'down' },
+    { x: position.x, y: position.y - 1, direction: 'up' },
+  ];
+
+  adjacentPipes = adjacentPipes.filter(({ x, y, direction }) => {
+    if (x < 0 || y < 0) {
+      return false;
+    }
+    const potentialPipe = grid.at(y)?.at(x);
+    const potentialDirections = potentialPipe
+      ? pipeMap[potentialPipe]
+      : undefined;
+    if (potentialDirections === undefined) {
+      return false;
+    }
+
+    return (
+      currentDirections.includes(direction) &&
+      potentialDirections.includes(oppositeDirection[direction])
+    );
+  });
+
+  return adjacentPipes.map(({ x, y }) => ({ x, y }));
 }
 
 function traceLongestPath({ startPosition, grid }: PipeGrid): number {
-  const tracedPositions: Vec2[] = [];
+  const tracedPositions: Vec2[] = [startPosition];
   let stepCount = 1;
-  // const visualizer = traceVisualizer({ startPosition, grid });
-  // console.log(startPosition);
+  // const visualizer = _traceVisualizer({ startPosition, grid });
 
   (function tracer(currentPosition: Vec2) {
     const adjacentPipes = findValidAdjacentPipes(grid, currentPosition);
@@ -106,7 +103,7 @@ function traceLongestPath({ startPosition, grid }: PipeGrid): number {
       if (!isPipeAleadyTraced) {
         // setTimeout(() => {
         //   visualizer.updateCellColor({ x, y }, 'fgWhite');
-        // }, stepCount * 250);
+        // }, stepCount);
 
         tracedPositions.push({ x, y });
         stepCount += 1;
@@ -118,7 +115,7 @@ function traceLongestPath({ startPosition, grid }: PipeGrid): number {
   return stepCount / 2;
 }
 
-function traceVisualizer({ grid }: PipeGrid) {
+function _traceVisualizer({ grid }: PipeGrid) {
   const colors = {
     reset: '\x1b[0m',
     bright: '\x1b[1m',
@@ -155,26 +152,32 @@ function traceVisualizer({ grid }: PipeGrid) {
   );
 
   function updateCellColor({ x, y }: Vec2, newColor: keyof typeof colors) {
-    buffer[x][y].color = colors[newColor];
-    process.stdout.clearLine(1);
-    process.stdout.cursorTo(0, 0);
-    drawBufferToCanvas();
+    buffer[y][x].color = colors[newColor];
+    process.stdout.cursorTo(x, y);
+    process.stdout.write(colors[newColor] + grid[y][x] + colors.reset);
   }
 
   function drawBufferToCanvas() {
+    process.stdout.cursorTo(0, 0);
     process.stdout.write(
       buffer.reduce(
         (accumulator, row) =>
           `${
             accumulator +
             row.map(({ color, cell }) => color + cell + colors.reset).join('')
-          }\n`,
+          } \n`,
         '',
       ),
     );
   }
 
   console.clear();
+  if (
+    process.stdout.columns >= grid.length &&
+    process.stdout.rows >= grid[0].length
+  ) {
+    drawBufferToCanvas();
+  }
 
   return { updateCellColor };
 }
