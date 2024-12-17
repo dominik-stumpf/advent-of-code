ed2faf5cee5a859c721ecca7cefe0bdda2440a4c
Dominik Stumpf <s.dominik.work@gmail.com>
2024-12-17 13:56:27 +0100

    feat: solve day 2 part 2 using guide

--- rust/year_2022/src/bin/day_02_part_2.rs
+++ rust/year_2022/src/bin/day_02_part_2.rs
@@ -18,10 +18,23 @@ impl TryFrom<char> for Move {
 
     fn try_from(c: char) -> Result<Self, Self::Error> {
         match c {
-            'A' | 'X' => Ok(Move::Rock),
-            'B' | 'Y' => Ok(Move::Paper),
-            'C' | 'Z' => Ok(Move::Scissors),
+            'A' => Ok(Move::Rock),
+            'B' => Ok(Move::Paper),
+            'C' => Ok(Move::Scissors),
             _ => Err(color_eyre::eyre::eyre!("not a valid move: {c:?}")),
+        }
+    }
+}
+
+impl TryFrom<char> for Outcome {
+    type Error = color_eyre::Report;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'X' => Ok(Outcome::Loss),
+            'Y' => Ok(Outcome::Draw),
+            'Z' => Ok(Outcome::Win),
+            _ => Err(color_eyre::eyre::eyre!("not a valid outcome: {c:?}")),
         }
     }
 }
@@ -31,26 +44,26 @@ impl FromStr for Round {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut chars = s.chars();
-        let (Some(theirs), Some(' '), Some(ours), None) =
+        let (Some(theirs), Some(' '), Some(outcome), None) =
             (chars.next(), chars.next(), chars.next(), chars.next())
         else {
             return Err(color_eyre::eyre::eyre!(
-                "expected <theirs>SP<ours>EOF, got {s:?}"
+                "expected <theirs>SP<outcome>EOF, got {s:?}"
             ));
         };
+        let theirs = Move::try_from(theirs)?;
+        let outcome = Outcome::try_from(outcome)?;
+        let ours = outcome.matching_move(theirs);
 
-        Ok(Self {
-            theirs: theirs.try_into()?,
-            ours: ours.try_into()?,
-        })
+        Ok(Self { theirs, ours })
     }
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Outcome {
-    Win,
-    Draw,
     Loss,
+    Draw,
+    Win,
 }
 
 impl Outcome {
@@ -59,6 +72,14 @@ impl Outcome {
             Outcome::Win => 6,
             Outcome::Draw => 3,
             Outcome::Loss => 0,
+        }
+    }
+
+    fn matching_move(self, theirs: Move) -> Move {
+        match self {
+            Outcome::Win => theirs.winning_move(),
+            Outcome::Draw => theirs.drawing_move(),
+            Outcome::Loss => theirs.losing_move(),
         }
     }
 }
@@ -83,13 +104,34 @@ impl Move {
         }
     }
 
-    /// How many points do we get for picking that move?
     fn inherent_points(self) -> usize {
         match self {
             Move::Rock => 1,
             Move::Paper => 2,
             Move::Scissors => 3,
         }
+    }
+
+    const ALL_MOVES: [Move; 3] = [Move::Rock, Move::Paper, Move::Scissors];
+
+    fn winning_move(self) -> Self {
+        Self::ALL_MOVES
+            .iter()
+            .copied()
+            .find(|m| m.beats(self))
+            .expect("at least one move beats us")
+    }
+
+    fn losing_move(self) -> Self {
+        Self::ALL_MOVES
+            .iter()
+            .copied()
+            .find(|&m| self.beats(m))
+            .expect("we beat at least one move")
+    }
+
+    fn drawing_move(self) -> Self {
+        self
     }
 }
 
@@ -112,8 +154,8 @@ fn main() -> color_eyre::Result<()> {
         include_str!("./input_02_part_1")
             .lines()
             .map(Round::from_str)
-            // 👇 this is provided by `Itertools`
-            .map_ok(|r| r.our_score()),
+            //    there 👇   👇
+            .map_ok(|r| dbg!(dbg!(r).our_score())),
         |it| it.sum(),
     )?;
     dbg!(total_score);
