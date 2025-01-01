2fb59c6bd9b3ad8047c4873ec1ce1c2d689334b5
Dominik Stumpf <s.dominik.work@gmail.com>
2025-01-01 15:25:25 +0100

    feat: solve day 4 part 1 spag

---
+++ rust/year_2022/src/bin/day_04_part_1.rs
@@ -0,0 +1,33 @@
+fn intersects(ranges: &[[usize; 2]; 2]) -> bool {
+    let [r1, r2] = ranges;
+    if r1[0] <= r2[0] && r1[1] >= r2[1] {
+        return true;
+    }
+    if r2[0] <= r1[0] && r2[1] >= r1[1] {
+        return true;
+    }
+    false
+}
+
+fn main() {
+    let input = include_str!("./input_04_part_1").trim();
+
+    let result = input
+        .lines()
+        .map(|line| {
+            line.split_once(',')
+                .expect("to have comma separator between ranges")
+        })
+        .map(|(first, second)| [first, second])
+        .map(|range| {
+            range
+                .map(|r| r.split_once('-').expect("ranges to be separated by commas"))
+                .map(|(r1, r2)| {
+                    [r1, r2].map(|v| v.parse::<usize>().expect("to be valid positive integer"))
+                })
+        })
+        .filter(intersects)
+        .count();
+
+    dbg!(result);
+}
