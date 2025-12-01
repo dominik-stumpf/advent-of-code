344a1762f990d0aed343d255f8ec65ad997ac5a0
Dominik Stumpf <s.dominik.work@gmail.com>
2025-12-02 00:44:22 +0100

    feat: solve day 1 part 2

--- go/year_2025/day_01/solver.go
+++ go/year_2025/day_01/solver.go
@@ -2,7 +2,6 @@ package day_01
 
 import (
 	_ "embed"
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -11,10 +10,6 @@ import (
 var Input string
 
 func SolvePartOne(input string) (result int) {
-	// fmt.Printf("%v\n", (100+50-68)%100) // 82
-	// fmt.Printf("%v\n", (100+82-30)%100) // 52
-	// fmt.Printf("%v\n", (100+52+48)%100) // 0
-	// fmt.Printf("%v\n", (100+0-5)%100)   // 95
 	dial := 50
 	for line := range strings.SplitSeq(input, "\n") {
 		direction := line[0]
@@ -22,19 +17,50 @@ func SolvePartOne(input string) (result int) {
 		if err != nil {
 			panic(err)
 		}
-		if direction == 'L' {
+		switch direction {
+		case 'L':
 			rotation = -rotation
+		case 'R':
+		default:
+			panic("failed to parse direction")
 		}
 		dial = (100 + dial + rotation) % 100
-		fmt.Printf("%q %d %d\n", direction, rotation, dial)
 		if dial == 0 {
 			result++
 		}
 	}
-	_ = dial
 	return
 }
 
 func SolvePartTwo(input string) (result int) {
+	dial := 50
+	for line := range strings.SplitSeq(input, "\n") {
+		direction := line[0]
+		rotation, err := strconv.Atoi(line[1:])
+		if err != nil {
+			panic(err)
+		}
+		result += rotation / 100
+		switch direction {
+		case 'L':
+			rotation = -rotation
+		case 'R':
+		default:
+			panic("failed to parse direction")
+		}
+		nextDial := dial + rotation%100
+		if nextDial <= 0 {
+			if nextDial < 0 {
+				nextDial += 100
+			}
+			if dial != 0 {
+				result++
+			}
+		} else if nextDial >= 100 {
+			nextDial -= 100
+			result++
+		}
+		dial = nextDial
+	}
 	return
 }
