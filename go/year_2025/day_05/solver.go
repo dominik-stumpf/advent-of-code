d0642ef14f1e094c481a70af2ceaab7a2106acd2
Dominik Stumpf <s.dominik.work@gmail.com>
2025-12-05 18:45:05 +0100

    feat: solve day 5 part 2

--- go/year_2025/day_05/solver.go
+++ go/year_2025/day_05/solver.go
@@ -3,6 +3,9 @@ package day_05
 import (
 	_ "embed"
 	"fmt"
+	"maps"
+	"math"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -26,9 +29,7 @@ func MustParseInput(input *string) (result Ingredients) {
 		panic("failed to parse input")
 	}
 	for line := range strings.SplitSeq(ranges, "\n") {
-		var left, right int
-		fmt.Sscanf(line, "%d-%d", &left, &right)
-		result.IDRanges = append(result.IDRanges, IDRange{left, right})
+		result.IDRanges = append(result.IDRanges, ParseIDRangeFromKey(line))
 	}
 	for line := range strings.SplitSeq(ids, "\n") {
 		id, err := strconv.Atoi(line)
@@ -40,8 +41,80 @@ func MustParseInput(input *string) (result Ingredients) {
 	return
 }
 
+func ParseIDRangeFromKey(key string) IDRange {
+	var left, right int
+	fmt.Sscanf(key, "%d-%d", &left, &right)
+	return IDRange{left, right}
+}
+
 func (rang IDRange) CheckIsInRange(id int) bool {
 	return rang.Left <= id && rang.Right >= id
+}
+
+func (rang IDRange) GetLength() int {
+	return rang.Right - rang.Left + 1
+}
+func (rang IDRange) GetKey() string {
+	return fmt.Sprintf("%d-%d", rang.Left, rang.Right)
+}
+
+func (ing *Ingredients) RemoveDuplicates() {
+	rangMap := map[string]IDRange{}
+	for _, rang := range ing.IDRanges {
+		rangMap[rang.GetKey()] = rang
+	}
+	ing.IDRanges = slices.Collect(maps.Values(rangMap))
+}
+
+func (ing *Ingredients) ReduceIDRanges() (result []IDRange) {
+	ing.RemoveDuplicates()
+	slices.SortFunc(ing.IDRanges, func(a, b IDRange) int {
+		return a.Left - b.Left
+	})
+	groups := map[string]int{}
+	var groupId int
+	for _, rang := range ing.IDRanges {
+		couldMerge := false
+		for _, rangEnd := range ing.IDRanges {
+			if rang == rangEnd {
+				continue
+			}
+			if rang.Left <= rangEnd.Right && rangEnd.Left <= rang.Right {
+				rangGroupId, ok := groups[rang.GetKey()]
+				rangEndGroupId, endOk := groups[rangEnd.GetKey()]
+				if ok && !endOk {
+					groups[rangEnd.GetKey()] = rangGroupId
+				} else if endOk && !ok {
+					groups[rang.GetKey()] = rangEndGroupId
+				} else if !ok && !endOk {
+					groups[rang.GetKey()] = groupId
+					groups[rangEnd.GetKey()] = groupId
+					groupId += 1
+				}
+				couldMerge = true
+				continue
+			}
+		}
+		if !couldMerge {
+			result = append(result, rang)
+		}
+	}
+	for i := range groupId {
+		groupOverlap := IDRange{Left: math.MaxInt, Right: 0}
+		for key, value := range groups {
+			if value == i {
+				rang := ParseIDRangeFromKey(key)
+				if rang.Left < groupOverlap.Left {
+					groupOverlap.Left = rang.Left
+				}
+				if rang.Right > groupOverlap.Right {
+					groupOverlap.Right = rang.Right
+				}
+			}
+		}
+		result = append(result, groupOverlap)
+	}
+	return
 }
 
 func SolvePartOne(input string) (result int) {
@@ -62,5 +135,9 @@ func SolvePartOne(input string) (result int) {
 }
 
 func SolvePartTwo(input string) (result int) {
+	ingredients := MustParseInput(&input)
+	for _, rang := range ingredients.ReduceIDRanges() {
+		result += rang.GetLength()
+	}
 	return
 }
